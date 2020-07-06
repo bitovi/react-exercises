df6e4414f2f085aaaa76afdc210dc81a1d26f5d0
Dan Crescimanno <dcrmls@gmail.com>
2020-07-06 15:29:12 -0400

    Updates to exercises

--- src/exercises/e - Responsive React/components/App.js
+++ src/exercises/e - Responsive React/components/App.js
@@ -2,5 +2,13 @@
 create three rectangles (border: '1px solid black') which will span the 
 full width of the screen if you are above 768px, but will "stack" when the
 width is beneath that*/
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Grid } from '@material-ui/core/Grid';
+
+// Your HTML goes here
+function App() {
+  return <div></div>;
+}
+
+export default App;
