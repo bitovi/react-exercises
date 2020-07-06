c45826a2682a68f0b542a9566197bc763c9b5429
Dan Crescimanno <dcrmls@gmail.com>
2020-07-06 15:56:04 -0400

    Better exercise

--- src/exercises/e - Responsive React/components/App.js
+++ src/exercises/e - Responsive React/components/App.js
@@ -1,14 +1,11 @@
 /* In this exercise you will use the Grid component in material ui to 
-create three rectangles (border: '1px solid black') which will span the 
-full width of the screen if you are above 768px, but will "stack" when the
-width is beneath that*/
+create three rectangles (border: '1px solid black') which will be side-by-side 
+when the screen is above 600px, but will "stack" when the width is beneath that*/
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Grid } from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 
-// Your HTML goes here
 function App() {
-  return <div></div>;
+  return <div />;
 }
 
 export default App;
