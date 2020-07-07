80fda249ee2b6746ae09ab793fd3415613f6ae0b
Dan Crescimanno <dcrmls@gmail.com>
2020-07-07 15:42:35 -0400

    More details about this exercise

--- src/exercises/f - Responsive React/_solutions/App.js
+++ src/exercises/f - Responsive React/_solutions/App.js
@@ -1,6 +1,9 @@
 /* In this exercise you will use the Grid component in material ui to 
 create three rectangles (border: '1px solid black') which will be side-by-side 
 when the screen is above 600px, but will "stack" when the width is beneath that*/
+
+/* How would you change it so that when the grid gets below 300px, we don't render 
+the grid at all? */
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
