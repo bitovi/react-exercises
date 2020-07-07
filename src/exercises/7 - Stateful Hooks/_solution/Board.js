53a2513e84319c9575bebd1a368eb8b1da300976
Christopher J Baker <christopher@bitovi.com>
2020-07-07 13:21:37 -0700

    add exercise for 7

---
+++ src/exercises/7 - Stateful Hooks/_solution/Board.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Square from '../../../app/components/Square';
+
+function Board() {
+  const [board, setBoard] = useState([ '', '', '', '', '', '', '', '', '' ]);
+  const [isXTurn, setIsXTurn] = useState(true);
+
+  function handleSquareClick(squareIndex) {
+    if (board[squareIndex]) {
+      return;
+    }
+
+    const newBoard = [ ...board ];
+    newBoard[squareIndex] = isXTurn ? 'X' : 'O';
+
+    setBoard(newBoard);
+    setIsXTurn((value) => !value);
+  }
+
+  return (
+    <div className="board">
+      {board.map((symbol, index) => (
+        <Square
+          key={index}
+          symbol={symbol}
+          onClick={() => handleSquareClick(index)}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/exercises/7 - Stateful Hooks/components/Board.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Square from '../../../app/components/Square';
+
+function Board() {
+  const board = []
+  // store the board in state
+  // store the current player in state
+
+  function handleSquareClick(square) {
+    // update the board
+    // update the current player
+  }
+
+  return (
+    <div className="board">
+      {board.map((symbol, index) => (
+        <Square
+          key={index}
+          symbol={symbol}
+          onClick={() => handleSquareClick(index)}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Board;
