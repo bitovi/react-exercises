c89c95d6c40b196b4c3ad14da39db3b3d75c64b8
mikedane <mikedane94@gmail.com>
2020-08-04 09:44:04 +1000

    Updates

---
+++ precache-manifest.00b115f4542dfdfee3239ae32378f8a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1378b8e43a97a46b2783b964037ec415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73919c3be107995307f7",
+    "url": "/static/css/main.59b77230.chunk.css"
+  },
+  {
+    "revision": "8a01f7de7ac40fc9df48",
+    "url": "/static/js/2.963df22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.963df22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73919c3be107995307f7",
+    "url": "/static/js/main.bf931c9f.chunk.js"
+  },
+  {
+    "revision": "54c286af65855f2610d9",
+    "url": "/static/js/runtime-main.5bc8014b.js"
+  }
+]);
